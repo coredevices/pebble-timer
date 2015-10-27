ebd8f86bef8afb5c7a4114173c05da84094569f7
Eric Phillips <eric.d.phillips@icloud.com>
2015-10-27 19:43:19 -0400

    Remove old "bubble" code and remove water line on aplite

--- src/detail_window.c
+++ src/detail_window.c
@@ -34,38 +34,13 @@
  #include "detail_window.h"
  #include "countdown_timer.h"
 
- #define BUBBLE_DELTA 60
- #define BUBBLE_MAX_RADIUS 10
- #define BUBBLE_MIN_RADIUS 4
- #define BUBBLE_MAX_DELAY_MS 2000
- #define BUBBLE_MIN_DELAY_MS 600
-
  #define TEXT_LAYER_MAX_LARGE_CHARACTERS 5
-
-#ifdef PBL_COLOR
-#define BUBBLE_FILL_COLOR GColorCeleste
-#define BUBBLE_POPPED_FILL_COLOR GColorLightGray
-#else
-#define BUBBLE_FILL_COLOR GColorWhite
-#define BUBBLE_POPPED_FILL_COLOR GColorWhite
-#endif
 
 
 
 /*******************************************************************************
  * STRUCTURE DEFINITION
  */
-
- /*
-  * the structure of a bubble for the animations
-  */
-
-typedef struct Bubble {
-  GPoint      pt1;    //< actual point of bubble, moves side to side
-  GPoint      pt2;    //< master point of bubble, moves straight up from bottom
-  uint16_t    rad;    //< radius of bubble
-  bool        live;   //< whether the bubble has popped or not
-} Bubble;
 
 /*
  * the structure of a DetailWindow
@@ -82,16 +57,12 @@ struct DetailWindow {
   GColor      highlight_color;        //< main color for highlights
 #ifdef PBL_SDK_3
   StatusBarLayer *status;             //< status bar for SDK 3
-#else
-  GBitmap     *waves_image;           //< image for Aplite water level
 #endif
   DetailWindowCallbacks callbacks;    //< callbacks for button presses
 
   char        main_buff[12];          //< text buffer for main_text
   char        sub_buff[12];           //< text buffer for sub_text
 
-  Bubble      bubbles[4];                 //< array of Bubble structures
-  int64_t     bubble_last_release;        //< time since last bubble release
   bool        animation_update_needed;    //< whether it needs to be refreshed
   bool        power_saver_mode;           //< whether it is in power saver mode or not
 
@@ -105,141 +76,6 @@ struct DetailWindow {
  */
 
 /*
- * draw a thick line regardless of platform
- */
-
-static void draw_thick_line(GContext *ctx, GPoint p0, GPoint p1) {
-#ifdef PBL_SDK_3
-  graphics_draw_line(ctx, p0, p1);
-#else
-  graphics_draw_line(ctx, p0, p1);
-  graphics_draw_line(ctx, GPoint(p0.x + 1, p0.y), GPoint(p1.x + 1, p1.y));
-  graphics_draw_line(ctx, GPoint(p0.x - 1, p0.y), GPoint(p1.x - 1, p1.y));
-  graphics_draw_line(ctx, GPoint(p0.x, p0.y + 1), GPoint(p1.x, p1.y + 1));
-  graphics_draw_line(ctx, GPoint(p0.x, p0.y - 1), GPoint(p1.x, p1.y - 1));
-#endif
-}
-
-
-
-/*
- * draw bubbles on graphics context
- */
-
-static void draw_bubbles(DetailWindow *detail_window, GContext *ctx, int16_t level) {
-  // set up drawing environment
-  graphics_context_set_stroke_color(ctx, GColorBlack);
-#ifdef PBL_SDK_3
-  graphics_context_set_stroke_width(ctx, 3);
-#endif
-
-  Bubble *t_bubble = detail_window->bubbles;
-  for (uint8_t ii = 0; ii < ARRAY_LENGTH(detail_window->bubbles); ii++, t_bubble++) {
-    // check if on screen
-    if (t_bubble->live){
-      // check if popped
-      if (t_bubble->pt2.y > level){
-        graphics_context_set_fill_color(ctx, GColorBlack);
-        graphics_fill_circle(ctx, t_bubble->pt1, t_bubble->rad);
-        graphics_context_set_fill_color(ctx, BUBBLE_FILL_COLOR);
-        graphics_fill_circle(ctx, t_bubble->pt1, t_bubble->rad - 3);
-      }
-      else if (level < t_bubble->pt2.y + 15){
-      // draw divot
-        graphics_context_set_fill_color(ctx, BUBBLE_POPPED_FILL_COLOR);
-        graphics_fill_circle(ctx, GPoint(t_bubble->pt1.x, t_bubble->pt2.y), t_bubble->rad - 2);
-        // draw lines
-        for (uint8_t jj = 0; jj < 4; jj++){
-          // calculate based on angle
-          GPoint pt1 = GPoint(t_bubble->pt1.x + (level - t_bubble->pt2.y + t_bubble->rad) *
-            cos_lookup(jj * TRIG_MAX_ANGLE / 8 + TRIG_MAX_ANGLE / 16) / TRIG_MAX_RATIO,
-            t_bubble->pt1.y - (level - t_bubble->pt2.y + t_bubble->rad) *
-            sin_lookup(jj * TRIG_MAX_ANGLE / 8 + TRIG_MAX_ANGLE / 16) / TRIG_MAX_RATIO);
-          GPoint pt2 = GPoint(t_bubble->pt1.x + (level - t_bubble->pt2.y + t_bubble->rad + 5) *
-            cos_lookup(jj * TRIG_MAX_ANGLE / 8 + TRIG_MAX_ANGLE / 16) / TRIG_MAX_RATIO,
-            t_bubble->pt1.y - (level - t_bubble->pt2.y + t_bubble->rad + 5) *
-            sin_lookup(jj * TRIG_MAX_ANGLE / 8 + TRIG_MAX_ANGLE / 16) / TRIG_MAX_RATIO);
-          draw_thick_line(ctx, pt1, pt2);
-        }
-      }
-    }
-  }
-}
-
-
-
-/*
- * ease in/out quadratic
- *
- * returns a value that oscillates like a sign wave
- */
-
-static int32_t ease_in_out_quad(int32_t oT, int32_t b, int32_t c, int32_t d) {
-  // this is a standard easing algorithm that can be found online
-  // simply search for "ease in out quad" and you can find many examples
-  int32_t t = oT * 1000;
-  t /= d / 2;
-  if (t < 1000) {
-    return c / 2 * t * t / 1000000 + b;
-  }
-  t -= 1000;
-  return -c/2 * (t*(t-2000) - 1000000) / 1000000 + b;
-};
-
-
-
-/*
- * step bubbles
- *
- * increment the bubbles position and state so that they move side
- * to side and upward
- */
-
-static void step_bubbles(DetailWindow *detail_window, int16_t level) {
-  int64_t epoch = countdown_timer_get_epoch_ms();
-  GRect bounds = layer_get_bounds(detail_window->layer);
-
-  detail_window->animation_update_needed =
-    !countdown_timer_get_paused(detail_window->countdown_timer);
-
-  Bubble *t_bubble = detail_window->bubbles;
-  for (uint8_t ii = 0; ii < ARRAY_LENGTH(detail_window->bubbles); ii++, t_bubble++){
-    // check if on screen
-    if (t_bubble->live){
-      // move bubble
-      if (t_bubble->pt2.y > level){
-        t_bubble->pt1.y -= 3;
-        t_bubble->pt1.x = ease_in_out_quad(abs(t_bubble->pt1.y %
-          (BUBBLE_DELTA * 2) - BUBBLE_DELTA), t_bubble->pt2.x, 15, BUBBLE_DELTA);
-      }
-      t_bubble->pt2.y -= 3;
-      // check if dead
-      if (t_bubble->pt2.y < 0){
-        // set bubble to be dead
-        t_bubble->live = false;
-      }
-      else { // animation steps are still needed
-        detail_window->animation_update_needed = true;
-      }
-    }
-    else if (epoch > detail_window->bubble_last_release &&
-             !countdown_timer_get_paused(detail_window->countdown_timer)) {
-      // create bubble
-      t_bubble->rad = rand() % (BUBBLE_MAX_RADIUS - BUBBLE_MIN_RADIUS) + BUBBLE_MIN_RADIUS;
-      // set bubble last time
-      detail_window->bubble_last_release = epoch +
-        rand() % (BUBBLE_MAX_DELAY_MS - BUBBLE_MIN_DELAY_MS) + BUBBLE_MIN_DELAY_MS;
-      // continue creation
-      t_bubble->pt1 = t_bubble->pt2 = GPoint(rand() % (bounds.size.w - ACTION_BAR_WIDTH) -
-        BUBBLE_DELTA / 5, bounds.size.h + t_bubble->rad * 2);
-      t_bubble->live = true;
-    }
-  }
-}
-
-
-
-/*
  * layer update proc
  * for animating bubbles in background
  */
@@ -249,8 +85,8 @@ static void layer_update_proc(Layer *layer, GContext *ctx) {
   DetailWindow *detail_window = (*(DetailWindow**)layer_get_data(layer));
   int64_t current_time = countdown_timer_get_current_time(detail_window->countdown_timer);
   int64_t total_time = countdown_timer_get_duration(detail_window->countdown_timer);
-  int16_t water_level = layer_get_bounds(layer).size.h - layer_get_bounds(layer).size.h * current_time /
-    total_time;
+  int16_t water_level = layer_get_bounds(layer).size.h - layer_get_bounds(layer).size.h *
+    current_time / total_time;
 
   // draw background
 #ifdef PBL_ROUND
@@ -258,40 +94,11 @@ static void layer_update_proc(Layer *layer, GContext *ctx) {
   GRect bounds = layer_get_bounds(layer);
   graphics_fill_radial(ctx, bounds, GOvalScaleModeFitCircle, bounds.size.w / 2,
     TRIG_MAX_ANGLE - TRIG_MAX_ANGLE * current_time / total_time, TRIG_MAX_ANGLE);
-  // TODO: Fix this code
 #elif PBL_COLOR
   graphics_context_set_fill_color(ctx, detail_window->highlight_color);
   graphics_fill_rect(ctx, GRect(0, water_level, layer_get_bounds(layer).size.w,
     layer_get_bounds(layer).size.h - water_level), 1, GCornerNone);
-#else
-  graphics_context_set_fill_color(ctx, GColorBlack);
-  graphics_fill_rect(ctx, GRect(0, water_level, layer_get_bounds(layer).size.w, 2), 1, GCornerNone);
-//  GRect img_frame = gbitmap_get_bounds(detail_window->waves_image);
-//  img_frame.size.w = layer_get_bounds(layer).size.w;
-//  img_frame.origin = GPoint(0, water_level - img_frame.size.h);
-//  graphics_draw_bitmap_in_rect(ctx, detail_window->waves_image, img_frame);
 #endif
-
-
-//  if (!detail_window->power_saver_mode) {
-//    // step bubbles
-//    step_bubbles(detail_window, water_level);
-//    // draw bubbles
-//    draw_bubbles(detail_window, ctx, water_level);
-//  }
-//  else {
-//    // draw text
-//    graphics_context_set_text_color(ctx, GColorBlack);
-//#ifdef PBL_SDK_3
-//    graphics_draw_text(ctx, "Power Saver", fonts_get_system_font(FONT_KEY_GOTHIC_14),
-//      GRect(0, 60, layer_get_bounds(detail_window->layer).size.w - ACTION_BAR_WIDTH, 35),
-//      GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
-//#else
-//    graphics_draw_text(ctx, "Power Saver", fonts_get_system_font(FONT_KEY_GOTHIC_14),
-//      GRect(0, 44, layer_get_bounds(detail_window->layer).size.w - ACTION_BAR_WIDTH, 35),
-//      GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
-//#endif
-//  }
 }
 
 
@@ -392,9 +199,6 @@ detail_window->callbacks = detail_window_callbacks;
   detail_window->play_icon = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PLAY);
   detail_window->pause_icon = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PAUSE);
   detail_window->delete_icon = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_DELETE);
-#ifndef PBL_COLOR
-  detail_window->waves_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_WAVES);
-#endif
   detail_window->large_font = fonts_load_custom_font(
     resource_get_handle(RESOURCE_ID_FONT_LECO_REGULAR_SUBSET_36));
   detail_window->medium_font = fonts_load_custom_font(
@@ -403,8 +207,6 @@ detail_window->callbacks = detail_window_callbacks;
     resource_get_handle(RESOURCE_ID_FONT_LECO_REGULAR_SUBSET_20));
   // zero some values
   detail_window->countdown_timer = NULL;
-  memset(detail_window->bubbles, 0, sizeof(detail_window->bubbles));
-  detail_window->bubble_last_release = 0;
   // get window parameters
   Layer *root = window_get_root_layer(detail_window->window);
   GRect bounds = layer_get_frame(root);
@@ -496,9 +298,6 @@ void detail_window_destroy(DetailWindow *detail_window) {
     gbitmap_destroy(detail_window->play_icon);
     gbitmap_destroy(detail_window->pause_icon);
     gbitmap_destroy(detail_window->delete_icon);
-#ifndef PBL_COLOR
-    gbitmap_destroy(detail_window->waves_image);
-#endif
     fonts_unload_custom_font(detail_window->large_font);
     fonts_unload_custom_font(detail_window->medium_font);
     fonts_unload_custom_font(detail_window->small_font);
